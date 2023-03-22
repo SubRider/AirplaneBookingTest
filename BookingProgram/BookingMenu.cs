b706c830c642a4e7b01a60a7e100a61c30c268a0
SubRider <1063617@hr.nl>
2023-03-22 13:57:28 +0100

    Template Added

--- BookingProgram/BookingMenu.cs
+++ BookingProgram/BookingMenu.cs
@@ -1,7 +1,32 @@
 ﻿static class BookingMenu
 {
+    private static ButtonTemplate StartButton = new(ConsoleColor.Green, "Start", "start");
+    private static ButtonTemplate ExitButton = new(ConsoleColor.DarkRed, "Exit", "exit");
+    public static bool Quit = false;
+
     static void Main()
     {
-
+        StartScreen();
+        while (!Quit)
+        {
+            InputChecker.CheckInput(false);
+        }
     }
+    public static void StartScreen()
+    {
+        Console.ResetColor();
+        ToASCIIArt.Write("Rotterdam");
+        ToASCIIArt.Write("Airlines", 1);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Button start = new(StartButton, 21);
+        Button exit = new(ExitButton, 22);
+        start.Show();
+        exit.Show();
+    }
+    public static void MainMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome [user]");
+    }
+}
 }
---
+++ BookingProgram/InputChecker.cs
@@ -0,0 +1,38 @@
+﻿static class InputChecker
+{
+    public static void CheckInput(bool leftRight)
+    {
+        if (Console.KeyAvailable)
+        {
+            ConsoleKey input = Console.ReadKey(true).Key;
+            int cursorTop = Console.GetCursorPosition().Top;
+            int cursorLeft = Console.GetCursorPosition().Left;
+            while (Console.KeyAvailable)
+            {
+                Console.ReadKey(true);
+            }
+            if (input == ConsoleKey.Escape) ConsoleApp.Quit = true;
+            if (input == ConsoleKey.Enter)
+            {
+                int index = Button.ButtonYLocations.IndexOf(cursorTop);
+                if (index != -1) Button.Buttons[index].Activate();
+                index = Button.ButtonLocations.IndexOf((cursorLeft, cursorTop));
+                if (index != -1) Button.Buttons[index].Activate();
+            }
+            else if (!leftRight)
+            {
+                if (input == ConsoleKey.W || input == ConsoleKey.UpArrow) cursorTop--;
+                else if (input == ConsoleKey.S || input == ConsoleKey.DownArrow) cursorTop++;
+            }
+            else if (leftRight)
+            {
+                if (input == ConsoleKey.W || input == ConsoleKey.UpArrow) cursorTop--;
+                else if (input == ConsoleKey.S || input == ConsoleKey.DownArrow) cursorTop++;
+                else if (input == ConsoleKey.A || input == ConsoleKey.LeftArrow) cursorLeft--;
+                else if (input == ConsoleKey.D || input == ConsoleKey.RightArrow) cursorLeft++;
+            }
+            Console.SetCursorPosition(cursorLeft, cursorTop);
+        }
+        return;
+    }
+}
