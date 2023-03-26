60e9c9a70487cb89b016237460b673b350a39afc
SubRider <1063617@hr.nl>
2023-03-26 15:04:05 +0200

    working on Seats and Airplane view

--- BookingProgram/Plane.cs
+++ BookingProgram/Plane.cs
@@ -1,11 +1,44 @@
 ﻿class Plane
 {
-    public int NumberOfSeats { get; set; }
     public string Model { get; set; }
+    public List<Seat> FirstClassSeats { get; set; }
+    public List<Seat> BusinessClassSeats { get; set; }
+    public List<Seat> EconomyClassSeats { get; set; }
 
-    public Plane(int numberOfSeats, string model)
+    public Plane(string model)
     {
-        NumberOfSeats = numberOfSeats;
         Model = model;
+        FirstClassSeats = LoadSeats(6, 20, 0, "First");
+        BusinessClassSeats = LoadSeats(6, 10, 20, "Business");
+        EconomyClassSeats = LoadSeats(6, 20, 30, "Economy");
     }
+    public void ShowSeats()
+    {
+        foreach (Seat seat in FirstClassSeats)
+        {
+            int cursorX = seat.RowNumber;
+            int cursorY = seat.SeatNumber;
+            Console.SetCursorPosition(cursorX - 1,cursorY - 1);
+            ConsoleColor seatColor = (seat.Booked) ? ConsoleColor.Red : ConsoleColor.Green;
+            Console.ForegroundColor = seatColor;
+            Console.Write(seat);
+        }
+        Console.ResetColor();
+    }
+    public List<Seat> LoadSeats(int rowSize, int amountOfRows, int prevClassRows, string _class)
+    {
+        //Add getSeatsFromJson
+        List<Seat> seats = new();
+        for (int row = 1; row <= amountOfRows; row++)
+        {
+            for (int seatNumber = 1; seatNumber <= rowSize; seatNumber++)
+            {
+                Seat seat = new(_class, row + prevClassRows, seatNumber);
+                seats.Add(seat);
+            }
+        }
+        return seats;
+    }
+
+
 }
--- BookingProgram/Seat.cs
+++ BookingProgram/Seat.cs
@@ -3,12 +3,18 @@
     public string Class { get; set; }
     public int RowNumber { get; set; }
     public char SeatLetter { get; set; }
+    public int SeatNumber { get; set; }
 
-    public Seat(string _class, int rowNumber, char seatLetter)
+    public bool Booked { get; set; }
+
+    public Seat(string _class, int rowNumber, int seatNumber)
     {
+        List<char> seatLetters = new() { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
         Class = _class;
         RowNumber = rowNumber;
-        SeatLetter = seatLetter;
+        SeatNumber = seatNumber;
+        SeatLetter = seatLetters[seatNumber-1];
+        Booked = false;
     }
 
 }
