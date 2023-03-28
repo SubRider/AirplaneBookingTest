cc900e0cb61f5e67cf40685ef0c87a5bbbbcf1d2
Jason <jasonvuong@live.nl>
2023-03-28 20:51:27 +0200

    Adding SignIn Function

---
+++ BookingProgram/CreateAccount.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+class CreateAccount
+{
+    public static void userInfo()
+    {
+        Console.Write("Enter account ID: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter email address: ");
+        string emailAddress = Console.ReadLine();
+
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+
+        Console.Write("Enter full name: ");
+        string fullName = Console.ReadLine();
+
+        
+        AccountModel account = new AccountModel(id, emailAddress, password, fullName);
+        
+        AccountsLogic accountlogic = new AccountsLogic();
+
+        // deze line zou iest anders moeten returnen 
+        accountlogic.UpdateList(account);  
+        return;
+    }
+}
