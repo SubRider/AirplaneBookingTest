633ee09fb9bd75b53428c3fb437fa863400c23f1
henri_078 <hj.vdstelt@gmail.com>
2023-05-17 00:14:00 +0200

    calendar kind of implemented

--- BookingProgram/BookingMenu.cs
+++ BookingProgram/BookingMenu.cs
@@ -48,6 +48,7 @@ static class BookingMenu
         _ = new Button("My Flights", 0, 2, menuBar, "left", () => History());
         _ = new Button("Account", 0, 3, menuBar, "left", () => AccountMenu());
         _ = new Button("Info", 0, 4, menuBar, "left", () => AirlineInfo());
+        _ = new Button("Cal", 0, 5, menuBar, "left", () => CalendarMenu(DateTime.Now.Month, DateTime.Now.Year));
     }
     public static void StartScreen()
     {
@@ -476,6 +477,32 @@ static class BookingMenu
         AddMenuBar(w1);
         MenuUpdated = true;
     }
+
+    public static void CalendarMenu(int month, int year)
+    {
+        int minYear = 1;
+        int maxYear = 9999;
+
+        Renderer.Clear();
+        Window w1 = new(1, 0.85);
+        w1.Text += $" {Calendar.PrintCal(year, month, minYear, maxYear)}";
+        
+        if (month == 12)
+        {
+            _ = new Button("Previous", 1, 1, w1, "bottom", () => CalendarMenu(month - 1, year));
+            _ = new Button("Next", 1, 40, w1, "bottom", () => CalendarMenu(1, year + 1));
+        }
+        else if (month == 1)
+        {
+            _ = new Button("Previous", 1, 1, w1, "bottom", () => CalendarMenu(12, year - 1));
+            _ = new Button("Next", 1, 40, w1, "bottom", () => CalendarMenu(month + 1, year));
+        }
+        else 
+        {
+            _ = new Button("Previous", 1, 1, w1, "bottom", () => CalendarMenu(month - 1, year));
+            _ = new Button("Next", 1, 40, w1, "bottom", () => CalendarMenu(month + 1, year));
+        }
+        AddMenuBar(w1);
+        MenuUpdated = true;
+    }
 }
-
-
--- BookingProgram/Calendar.cs
+++ BookingProgram/Calendar.cs
@@ -1,26 +1,18 @@
 static class Calendar
 {
-    public static void PrintCal(int year, int month)
+    public static string PrintCal(int year, int month, int minYear, int maxYear)
     {
-        int minYear = 2010;
-        int maxYear = 2060;
-
         Console.Clear();
+
         // validate year and month
         if (year < minYear || year > maxYear)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Invalid year. Please enter a year between 2023 and 2025.");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            return;
+            return $"Invalid year. Please enter a year between {minYear} and {maxYear}.";
         }
 
         if (month < 1 || month > 12)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Invalid month. Please enter a valid month between 1 and 12.");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            return;
+            return "Invalid month. Please enter a valid month between 1 and 12.";
         }
 
         // determine first- and last day of month
@@ -30,82 +22,99 @@ static class Calendar
 
         int lastDayOfMonth = DateTime.DaysInMonth(year, month);
 
+        DateTime currentDate = DateTime.Now;
+        int currentDay = currentDate.Day;
+        int currentMonth = currentDate.Month;
+        int currentYear = currentDate.Year;
+
         // print calendar
-        Console.WriteLine($"{new DateTime(DateTime.Now.Year, month, 1).ToString("MMMM")}, {year}\n");
-        Console.WriteLine("|   Mo   Tu   We   Th   Fr   Sa   Su");
-        Console.WriteLine("|-------------------------------------");
+        string calendar = "\n";
+        calendar += $"    {(new DateTime(DateTime.Now.Year, month, 1).ToString("MMMM")).ToUpper()}, {year}\n\n" +
+                    "   |   Mo   Tu   We   Th   Fr   Sa   Su\n" +
+                    "   |-------------------------------------\n";
         for (int i = 0; i < 6; i++)
         {
-            Console.Write("|");
+            calendar += "   |";
             for (int j = 1 - firstDay; j < 8 - firstDay; j++)
             {
+                // check for current day
+                if (month == currentMonth && year == currentYear && j + (7 * i) == currentDay && j + (7 * i) < 10)
+                {
+                    calendar += $"   [{j + (7 * i)}]";
+                    continue;
+                } 
+                else if (month == currentMonth && year == currentYear && j + (7 * i) == currentDay && j + (7 * i) >= 10 && j + (7 * i) <= lastDayOfMonth)
+                {
+                    calendar += $"  [{j + (7 * i)}]";
+                    continue;
+                }
+
+                // check if day has already passed
+                if (((j + (7 * i) < currentDay && month <= currentMonth && year <= currentYear) || (month < currentMonth && year <= currentYear) || (year < currentYear)) && (j + (7 * i) > lastDayOfMonth || j + (7 * i) < 1))
+                {
+                    calendar += $"     ";
+                    continue;
+                }
+                else if (((j + (7 * i) < currentDay && month <= currentMonth && year <= currentYear) || (month < currentMonth && year <= currentYear) || (year < currentYear)) && j + (7 * i) < 10)
+                {
+                    calendar += $"    \u001b[90m{j + (7 * i)}\u001b[0m";
+                    continue;
+                } 
+                else if (((j + (7 * i) < currentDay && month <= currentMonth && year <= currentYear) || (month < currentMonth && year <= currentYear) || (year < currentYear)) && j + (7 * i) >= 10 && j + (7 * i) <= lastDayOfMonth)
+                {
+                    calendar += $"   \u001b[90m{j + (7 * i)}\u001b[0m";
+                    continue;
+                }
+
+                // print the non current days
                 if (j + (7 * i) > lastDayOfMonth || j + (7 * i) < 1)
                 {
-                    Console.Write("     ");
+                    calendar += "     ";
                 }
                 else if (j + (7 * i) < 10)
                 {
-                    Console.Write($"    {j + (7 * i)}");
+                    calendar += $"    {j + (7 * i)}";
                 }
                 else
                 {
-                    Console.Write($"   {j + (7 * i)}");
+                    calendar += $"   {j + (7 * i)}";
                 }
             }
-            Console.WriteLine("\n|");
+            calendar += "\n   |\n";
         }
-
-        // page controls
-        bool quit = false;
-        while (!quit)
-        {
-            ConsoleKeyInfo keyInfo = Console.ReadKey();
-            ConsoleKey key = keyInfo.Key;
-
-            if (key == ConsoleKey.RightArrow && month < 12)
-            {
-                PrintCal(year, month + 1);
-            }
-            else if (key == ConsoleKey.LeftArrow && month > 1)
-            {
-                PrintCal(year, month - 1);
-            }
-            else if (key == ConsoleKey.RightArrow && month == 12)
-            {
-                PrintCal(year + 1, 1);
-            }
-            else if (key == ConsoleKey.LeftArrow && month == 1)
-            {
-                PrintCal(year - 1, 12);
-            }
-            else if (key == ConsoleKey.UpArrow && year < maxYear)
-            {
-                PrintCal(year + 1, month);
-            }
-            else if (key == ConsoleKey.DownArrow && year > minYear)
-            {
-                PrintCal(year - 1, month);
-            }
-            else if (key == ConsoleKey.Q)
-            {
-                Console.Clear();
-                quit = true;
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"\n{key} is not a valid key");
-                Thread.Sleep(1000);
-                ClearLastLine();
-                Console.ForegroundColor = ConsoleColor.Gray;
-            }
-        }
+        return $" {calendar}";
     }
-    public static void ClearLastLine()
-    {
-        int currentLineCursor = Console.CursorTop;
-        Console.SetCursorPosition(0, currentLineCursor - 1);
-        Console.Write(new string(' ', Console.WindowWidth));
-        Console.SetCursorPosition(0, currentLineCursor - 2);
-    }
+
+    // public static string PageControl(string nextOrPrev, int month, int year, int minYear, int maxYear)
+    // {
+    //     if (nextOrPrev == "Current")
+    //     {
+    //         return PrintCal(year, month, minYear, maxYear);
+    //     }
+    //     else if (nextOrPrev == "Next" && month < 12)
+    //     {
+    //         return PrintCal(year, month + 1, minYear, maxYear);
+    //     }
+    //     else if (nextOrPrev == "Prev" && month > 1)
+    //     {
+    //         return PrintCal(year, month - 1, minYear, maxYear);
+    //     }
+    //     else if (nextOrPrev == "Next" && month == 12)
+    //     {
+    //         return PrintCal(year + 1, 1, minYear, maxYear);
+    //     }
+    //     else if (nextOrPrev == "Prev" && month == 1)
+    //     {
+    //         return PrintCal(year - 1, 12, minYear, maxYear);
+    //     }
+    //     return "Invalid choice";
+        // else if (key == ConsoleKey.UpArrow && year < maxYear)
+        // {
+        //     PrintCal(year + 1, month, minYear, maxYear);
+        // }
+        // else if (key == ConsoleKey.DownArrow && year > minYear)
+        // {
+        //     PrintCal(year - 1, month, minYear, maxYear);
+        // }
+    // }
 }
