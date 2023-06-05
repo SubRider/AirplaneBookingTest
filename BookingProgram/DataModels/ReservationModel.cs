bc921ab495e2e71bd752469a5e99414e211cd7b0
bryandevuono <bryandevuono@gmail.com>
2023-06-05 09:59:19 +0200

    Canceling your flights is possible again

--- BookingProgram/DataModels/ReservationModel.cs
+++ BookingProgram/DataModels/ReservationModel.cs
@@ -15,4 +15,25 @@
         ReservationID = Reservations.Count;
         Reservations.Add(this);
     }
+
+    public void RemoveFlight(List<ReservationModel> Reservations, int flightID)
+    {
+        List<Seat> seatsCopy = new List<Seat>(BookingMenu.Seats);
+        foreach (Seat seat in seatsCopy)
+        {
+            BookingMenu.Seats.Remove(seat);
+        }
+        Reservations.RemoveAll(reservation => reservation.FlightID == flightID);
+        UpdateJsonData(Reservations);
+        Renderer.Clear();
+        Console.WriteLine("Succesfully canceled");
+        Thread.Sleep(2000);
+        BookingMenu.StartScreen();
+    }
+
+    public void UpdateJsonData(List<ReservationModel> reservations)
+    {
+        string path = "Reservations.json";
+        JsonCommunicator.Write(path, reservations);
+    }
 }
