dd966285f08fe6779537f961c3d59941c402326b
Marco <marco.speksnijder@gmail.com>
2023-06-16 20:01:08 +0200

    Seats are now connected to a flight instead of an airplane

--- BookingProgram/Flight.cs
+++ BookingProgram/Flight.cs
@@ -11,6 +11,11 @@ class Flight : IHasID
     public string DepartureDate { get; set; }
     public string ArrivalDate { get; set; }
     public int AirplaneID { get; set; }
+    public string Model { get; set; }
+    public List<Seat> Seats { get; set; }
+    public List<Seat> FirstClassSeats { get; set; }
+    public List<Seat> BusinessClassSeats { get; set; }
+    public List<Seat> EconomyClassSeats { get; set; }
 
     public Flight(string origin, string destination, string departureDate, string arrivalDate, int airplaneID)
     {
@@ -20,7 +25,46 @@ class Flight : IHasID
         Destination = destination;
         AirplaneID = airplaneID;
         ID = Flights.Count;
+        Model = Airplane.Planes[AirplaneID - 1].Model;
+        switch (Model)
+        {
+            case "737":
+                FirstClassSeats = LoadSeats(4, 4, 0, "First");
+                BusinessClassSeats = LoadSeats(6, 8, 4, "Business");
+                EconomyClassSeats = LoadSeats(6, 17, 12, "Economy");
+                break;
+            case "787":
+                FirstClassSeats = LoadSeats(6, 8, 0, "First");
+                BusinessClassSeats = LoadSeats(9, 9, 8, "Business");
+                EconomyClassSeats = LoadSeats(9, 14, 17, "Economy");
+                break;
+            case "A330":
+                FirstClassSeats = LoadSeats(6, 1, 0, "First");
+                BusinessClassSeats = LoadSeats(6, 3, 1, "Business");
+                EconomyClassSeats = LoadSeats(8, 33, 4, "Economy");
+                break;
+            default:
+                FirstClassSeats = LoadSeats(6, 5, 0, "First");
+                BusinessClassSeats = LoadSeats(6, 10, 5, "Business");
+                EconomyClassSeats = LoadSeats(6, 20, 15, "Economy");
+                break;
+        }
+        Seats = FirstClassSeats.Concat(BusinessClassSeats).Concat(EconomyClassSeats).ToList();
         Flights.Add(this);
+    }
+    
+    public List<Seat> LoadSeats(int rowSize, int amountOfRows, int prevClassRows, string _class)
+    {
+        List<Seat> seats = new();
+        for (int row = 1; row <= amountOfRows; row++)
+        {
+            for (int seatNumber = 1; seatNumber <= rowSize; seatNumber++)
+            {
+                Seat seat = new(_class, row + prevClassRows, seatNumber);
+                seats.Add(seat);
+            }
+        }
+        return seats;      
     }
 
     public static Flight FindByID(int id)
